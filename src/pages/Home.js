7b59a908100a1df5f978ffcb25fe6974aa3a451e
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-09-29 22:57:06 +0100

    implemented zx9 speaker session

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from "react";
 
 import Nav from "../components/Nav/Nav";
-import headphone from "../Assets/category-headphones/desktop/image-xx99-mark-one.jpg";
-import speaker from "../Assets/category-speakers/desktop/image-zx9.jpg";
-import earphone from "../Assets/category-earphones/desktop/image-yx1-earphones.jpg";
+import earphone from "../Assets/shared/desktop/image-earphones.png";
+import speaker from "../Assets/shared/desktop/image-speakers.png";
+import headphone from "../Assets/shared/desktop/image-headphones.png";
+import zx9_speakers from "../Assets/home/desktop/image-speaker-zx9.png";
 
 import classes from "./Home.module.css";
 
@@ -24,17 +25,39 @@ const Home = () => {
       </section>
 
       <section className={classes.card_container}>
-        <div className={classes.card}>
+        <div className={classes.card_content}>
           <img src={headphone} alt="headphone" />
-          <h2>HEADPHONES</h2>
+          <div className={classes.card}>
+            <h2>HEADPHONES</h2>
+          </div>
         </div>
-        <div className={classes.card}>
+
+        <div className={classes.card_content}>
           <img src={speaker} alt="speaker" />
-          <h2>SPEAKERS</h2>
+          <div className={classes.card}>
+            <h2>SPEAKERS</h2>
+          </div>
         </div>
-        <div className={classes.card}>
+
+        <div className={classes.card_content}>
           <img src={earphone} alt="earphone" />
-          <h2>EARPHONES</h2>
+          <div className={classes.card}>
+            <h2>EARPHONES</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className={classes.speaker}>
+        <div className={classes["zx9-speaker-img"]}>
+          <img src={zx9_speakers} alt="zx9_speakers" />
+        </div>
+        <div>
+          <h1>ZX9 SPEAKER</h1>
+          <p>
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <button> SEE PRODUCT</button>
         </div>
       </section>
     </Fragment>
