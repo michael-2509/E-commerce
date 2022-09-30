1359f5513dd95765b8a61e33377a4b8da4f4eec3
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-09-30 22:59:49 +0100

    Implemented zx9 and yx1 section on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import earphone from "../Assets/shared/desktop/image-earphones.png";
 import speaker from "../Assets/shared/desktop/image-speakers.png";
 import headphone from "../Assets/shared/desktop/image-headphones.png";
 import zx9_speakers from "../Assets/home/desktop/image-speaker-zx9.png";
+import yx1_earphone from "../Assets/home/desktop/image-earphones-yx1.jpg";
 
 import classes from "./Home.module.css";
 
@@ -34,6 +35,7 @@ const Home = () => {
 
         <div className={classes.card_content}>
           <img src={speaker} alt="speaker" />
+
           <div className={classes.card}>
             <h2>SPEAKERS</h2>
           </div>
@@ -50,14 +52,36 @@ const Home = () => {
       <section className={classes.speaker}>
         <div className={classes["zx9-speaker-img"]}>
           <img src={zx9_speakers} alt="zx9_speakers" />
+          {/* <div className={classes["speaker-image"]}></div> */}
         </div>
         <div>
-          <h1>ZX9 SPEAKER</h1>
+          <h1>
+            ZX9 <br /> SPEAKER
+          </h1>
           <p>
-            Upgrade to premium speakers that are phenomenally built to deliver
-            truly remarkable sound.
+            Upgrade to premium speakers that are
+            <br /> phenomenally built to deliver truly remarkable sound.
           </p>
           <button> SEE PRODUCT</button>
+        </div>
+      </section>
+
+      <section className={classes["section-ZX7"]}>
+        <div className={classes["speaker-ZX7"]}>
+          <h3>ZX7 SPEAKER</h3>
+          <button>SEE PRODUCT</button>
+        </div>
+      </section>
+
+      <section className={classes["section-yx1"]}>
+        <div className={classes["image-yx1"]}>
+          {" "}
+          <img src={yx1_earphone} alt="yx1_earphone" />
+        </div>
+
+        <div className={classes["yx1-content"]}>
+          <h3>YX1 EARPHONES</h3>
+          <button>SEE PPRODUCT</button>
         </div>
       </section>
     </Fragment>
