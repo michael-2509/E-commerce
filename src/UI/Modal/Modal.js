fc3f64b03eabee0c47bcb6c83fba83a3b79be075
Adigun Idowu Michael <idowumichael2509@gmail.com>
2022-10-20 21:34:14 +0100

    completed Modal style

--- src/UI/Modal/Modal.js
+++ src/UI/Modal/Modal.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const portalElement = document.getElementById("overlay");
+
 const Modal = (Props) => {
+  const Backdrop = () => {
+    return <div className={classes.backdrop}></div>;
+  };
+
+  const ModalOverlay = () => {
+    return <div className={classes.modal}>{Props.children}</div>;
+  };
+
   return (
     <>
-      <div className={classes.backdrop}>
-        <div className={classes.modal}>{Props.children}</div>
-      </div>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{Props.chidren}</ModalOverlay>,
+        portalElement
+      )}
     </>
   );
 };
--- src/components/Cart.js/Cart.js
+++ src/components/Cart.js/Cart.js
@@ -2,28 +2,31 @@ import React from "react";
 import classes from "./Cart.module.css";
 import Button from "../../UI/Button";
 import CartItem from "./CartItem";
+import Modal from "../../UI/Modal/Modal";
 
 const Cart = () => {
   return (
     <>
-      <section className={classes["cart-container"]}>
-        <div className={classes["cart-header"]}>
-          <h3>
-            Cart <span>(3)</span>
-          </h3>{" "}
-          <Button className={classes.button}>Remove all</Button>
-        </div>
+      <Modal>
+        <section className={classes["cart-container"]}>
+          <div className={classes["cart-header"]}>
+            <h3>
+              Cart <span>(3)</span>
+            </h3>{" "}
+            <Button className={classes.button}>Remove all</Button>
+          </div>
 
-        <CartItem />
-        <CartItem />
+          <CartItem />
+          <CartItem />
 
-        <div className={classes["cart-footer"]}>
-          <h3>Total</h3>
-          <p>$7400</p>
-        </div>
+          <div className={classes["cart-footer"]}>
+            <h3>Total</h3>
+            <p>$7400</p>
+          </div>
 
-        <Button>CHECKOUT</Button>
-      </section>
+          <Button>CHECKOUT</Button>
+        </section>
+      </Modal>
     </>
   );
 };
